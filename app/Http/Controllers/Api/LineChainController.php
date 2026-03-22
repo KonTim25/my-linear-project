1955d5747303cae4496d7cb57ef1e3ff7fec31da
Konstantin <ktimch@gmail.com>
2026-03-22 20:11:01 +0200

    Create LineChainController for managing line chain system

---
+++ app/Http/Controllers/Api/LineChainController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LineChainController extends Controller
+{
+    public function createLevel(Request $request) {
+        // Implementation here
+    }
+
+    public function addUserToChain(Request $request) {
+        // Implementation here
+    }
+
+    public function createTechnicalClone(Request $request) {
+        // Implementation here
+    }
+
+    public function createWorkingClone(Request $request) {
+        // Implementation here
+    }
+
+    public function promoteToNextLevel(Request $request) {
+        // Implementation here
+    }
+
+    public function completeChainPosition(Request $request) {
+        // Implementation here
+    }
+
+    public function skipChainPosition(Request $request) {
+        // Implementation here
+    }
+
+    public function getLevelStatus(Request $request) {
+        // Implementation here
+    }
+
+    public function getUserActiveChains(Request $request) {
+        // Implementation here
+    }
+
+    public function getChainParticipants(Request $request) {
+        // Implementation here
+    }
+
+    public function getTransactionHistory(Request $request) {
+        // Implementation here
+    }
+
+    public function closeUserLine(Request $request) {
+        // Implementation here
+    }
+}
