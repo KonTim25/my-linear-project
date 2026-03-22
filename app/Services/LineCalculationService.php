015a07dd9d2a47f9363879b3c394ef73fe9f5f86
Konstantin <ktimch@gmail.com>
2026-03-22 20:01:52 +0200

    Create LineCalculationService for calculation logic

---
+++ app/Services/LineCalculationService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class LineCalculationService
+{
+    // Add your calculation logic here
+
+    public function calculateLineLength(float $x1, float $y1, float $x2, float $y2): float
+    {
+        return sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
+    }
+
+    // Other calculation methods can be added.
+}
